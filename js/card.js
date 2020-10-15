2cfb65abef343a4b4a9f64fc125316ddd78dc30e
prisekin48 <prisekin48@gmail.com>
2020-10-15 17:41:31 +0300

    Создан card.js

---
+++ js/card.js
@@ -0,0 +1,92 @@
+'use strict';
+
+
+(() => {
+  window.card = {};
+    /**
+     * Inserts text data into an element if the data exists
+     * @param {string} text - taken string
+     * @param {object} target - link to the DOM-node
+     */
+  window.card.insertAndCheckTextData = (text, target) => {
+    if (text) {
+      target.textContent = text;
+    } else {
+      target.remove();
+    }
+  };
+  /**
+   * Renders .popup__features according to the given features from array
+   * @param {HTML-Node} template - Html-template for features
+   * @param {array} features - Array of features
+   *
+   */
+  window.card.renderFeatures = (template, features) => {
+    if (features.length > 0) {
+      let fragment = document.createDocumentFragment();
+
+      for (let feature of features) {
+        let element = template.querySelector(`.popup__feature`).cloneNode();
+        element.classList.value = `popup__feature popup__feature--${feature}`;
+        element.textContent = feature;
+        fragment.appendChild(element);
+      }
+      template.innerHTML = ``;
+      template.appendChild(fragment);
+    } else {
+      template.innerHTML = ``;
+    }
+  };
+  /**
+   * Renders photos according the given array of photos` links
+   * @param {HTML-Node} template - Html-template for photos
+   * @param {array} photos - Array of links
+   */
+  window.card.renderPhotos = (template, photos) => {
+    if (photos.length > 0) {
+      let fragment = document.createDocumentFragment();
+
+      for (let photo of photos) {
+        let element = template.querySelector(`.popup__photo`).cloneNode();
+        element.src = photo;
+        fragment.appendChild(element);
+      }
+      template.innerHTML = ``;
+      template.appendChild(fragment);
+    } else {
+      template.innerHTML = ``;
+    }
+  };
+  /** Prepares and renders .map__card element with ad`s data
+   *  @param {object} ad - An object with mocks data needed for cardElement filling
+   */
+  window.card.renderCard = (ad) => {
+    const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+    const cardElement = cardTemplate.cloneNode(true);
+    const closeButton = cardElement.querySelector('.popup__close');
+
+    window.card.insertAndCheckTextData(ad.offer.title, cardElement.querySelector(`.popup__title`));
+    window.card.insertAndCheckTextData(ad.offer.address, cardElement.querySelector(`.popup__text--address`));
+    window.card.insertAndCheckTextData(`${ad.offer.price}₽/ночь`, cardElement.querySelector(`.popup__text--price`));
+    window.card.insertAndCheckTextData(adMocksData.TYPES_DESCRIPTION[ad.offer.type], cardElement.querySelector(`.popup__type`));
+    window.card.insertAndCheckTextData(`${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`, cardElement.querySelector(`.popup__text--capacity`));
+    window.card.insertAndCheckTextData(`Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`, cardElement.querySelector(`.popup__text--time`));
+    window.card.renderFeatures(cardElement.querySelector(`.popup__features`), ad.offer.features);
+    window.card.insertAndCheckTextData(ad.offer.description, cardElement.querySelector(`.popup__description`));
+    window.card.renderPhotos(cardElement.querySelector(`.popup__photos`), ad.offer.photos);
+    cardElement.querySelector(`.popup__avatar`).src = ad.author.avatar;
+
+    closeButton.addEventListener('click', () => {
+      removeCurrentCard();
+    });
+    closeButton.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Enter') {
+        removeCurrentCard();
+      }
+    });
+
+    document.addEventListener('keydown', onDocumentEscPress);
+
+    document.querySelector(`.map__filters-container`).before(cardElement);
+  };
+})();
--- js/main.js
+++ js/main.js
@@ -9,14 +9,6 @@ const PinShifts = {
   Y: 70
 };
 
-
-// const ads = window.data.generateAds(window.data.AdsDataConsts.ADS_COUNT);
-
-
-
-
-
-
 const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
 
 /** Prepares and returns .map__pin element with adData
@@ -119,65 +111,14 @@ const onPinPressEnter = (evt, ad, pin) => {
 const onPinClick = (ad, pin) => {
   removeCurrentCard();
   setActivePin(pin);
-  renderCard(ad);
+  window.card.renderCard(ad);
 };
 
-/**
- * Inserts text data into an element if the data exists
- * @param {string} text - taken string
- * @param {object} target - link to the DOM-node
- */
-const insertAndCheckTextData = (text, target) => {
-  if (text) {
-    target.textContent = text;
-  } else {
-    target.remove();
-  }
-};
 
-/**
- * Renders .popup__features according to the given features from array
- * @param {HTML-Node} template - Html-template for features
- * @param {array} features - Array of features
- *
- */
-const renderFeatures = (template, features) => {
-  if (features.length > 0) {
-    let fragment = document.createDocumentFragment();
 
-    for (let feature of features) {
-      let element = template.querySelector(`.popup__feature`).cloneNode();
-      element.classList.value = `popup__feature popup__feature--${feature}`;
-      element.textContent = feature;
-      fragment.appendChild(element);
-    }
-    template.innerHTML = ``;
-    template.appendChild(fragment);
-  } else {
-    template.innerHTML = ``;
-  }
-};
 
-/**
- * Renders photos according the given array of photos` links
- * @param {HTML-Node} template - Html-template for photos
- * @param {array} photos - Array of links
- */
-const renderPhotos = (template, photos) => {
-  if (photos.length > 0) {
-    let fragment = document.createDocumentFragment();
 
-    for (let photo of photos) {
-      let element = template.querySelector(`.popup__photo`).cloneNode();
-      element.src = photo;
-      fragment.appendChild(element);
-    }
-    template.innerHTML = ``;
-    template.appendChild(fragment);
-  } else {
-    template.innerHTML = ``;
-  }
-};
+
 
 /**
  * Removes currently opened card if Escape is pressed
@@ -189,38 +130,7 @@ const onDocumentEscPress = (evt) => {
   }
 };
 
-/** Prepares and renders .map__card element with ad`s data
- *  @param {object} ad - An object with mocks data needed for cardElement filling
- */
-const renderCard = (ad) => {
-  const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
-  const cardElement = cardTemplate.cloneNode(true);
-  const closeButton = cardElement.querySelector('.popup__close');
 
-  insertAndCheckTextData(ad.offer.title, cardElement.querySelector(`.popup__title`));
-  insertAndCheckTextData(ad.offer.address, cardElement.querySelector(`.popup__text--address`));
-  insertAndCheckTextData(`${ad.offer.price}₽/ночь`, cardElement.querySelector(`.popup__text--price`));
-  insertAndCheckTextData(adMocksData.TYPES_DESCRIPTION[ad.offer.type], cardElement.querySelector(`.popup__type`));
-  insertAndCheckTextData(`${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`, cardElement.querySelector(`.popup__text--capacity`));
-  insertAndCheckTextData(`Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`, cardElement.querySelector(`.popup__text--time`));
-  renderFeatures(cardElement.querySelector(`.popup__features`), ad.offer.features);
-  insertAndCheckTextData(ad.offer.description, cardElement.querySelector(`.popup__description`));
-  renderPhotos(cardElement.querySelector(`.popup__photos`), ad.offer.photos);
-  cardElement.querySelector(`.popup__avatar`).src = ad.author.avatar;
-
-  closeButton.addEventListener('click', () => {
-    removeCurrentCard();
-  });
-  closeButton.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Enter') {
-      removeCurrentCard();
-    }
-  });
-
-  document.addEventListener('keydown', onDocumentEscPress);
-
-  document.querySelector(`.map__filters-container`).before(cardElement);
-};
 
 /**
  * -------------------------MODULE4-TASK1--------------------------------
